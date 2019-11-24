4de07eef7cfef4ffa9e58f2c3b619a60f3ef59ab
nathangong <nathangong1234@gmail.com>
2019-11-23 21:51:52 -0800

    Close dropdown menu if user clicks outside of it

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,21 @@ function dropdown() {
         opaque = true;
     }
 }
+
+window.onclick = function(event) {
+    if (!event.target.matches('.projects')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+        if (opaque) {
+            var projects = document.getElementById("projects");
+            projects.setAttribute("style", "none");
+            opaque = false;
+        }
+    }
+}
