5d48ad87b9efd1119f657225648fd4454feec0d4
nathangong <nathangong1234@gmail.com>
2019-11-28 13:30:50 -0800

    Classes Redesign

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -1,7 +1,7 @@
 function main(classes) {
     var d = new Date();
     var day = d.getDay(d.toString());
-    let element = document.getElementById("div2");
+    let element = document.getElementById("today");
 
     let titleNode = document.createTextNode("Today's Classes:");
     let title = document.createElement("h1");
@@ -38,55 +38,61 @@ function getPeriods(day) {
 }
 
 function getSchedule(classes) {
-    var node1 = document.createTextNode("Classes you have:");
-    var para1 = document.createElement("h3");
-    para1.appendChild(node1);
-    var element1 = document.getElementById("div2");
-    element1.appendChild(para1);
     for (i = 0; i < classes.length; i++) {
         var spaceNode = document.createTextNode(" ");
         var space = document.createElement("p");
         space.appendChild(spaceNode);
         var Break = document.createElement("br");
-        var checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        var id = "checkbox" + i;
-        checkbox.setAttribute("id", id);
-        var tommorow = new Date();
-        tommorow.setDate(tommorow.getDate() + 1);
-        tommorow.setHours(0, 0, 0, 0);
-        checkbox.setAttribute("onclick", "writeCookie(" + "\"checkbox" + i + "\", " + tommorow.toUTCString + ")");
-        var node = document.createTextNode(classes[i]);
-        var element = document.getElementById("div2");
+        var node = document.createTextNode("Period " + classes[i].charAt(classes[i].length-1) + " - " + classes[i].substring(0, classes[i].length-5));
+        node = "Period " + node.charAt(node.length()-1) + " - " + node.substring(0, node.length-5);
+        var element = document.getElementById("today");
         element.appendChild(node);
-        element.appendChild(checkbox);
         element.appendChild(Break);
         element.appendChild(space);
         element.appendChild(Break);
-
-        cookie = document.cookie;
-        if (cookie.includes(id)) {
-            index = cookie.lastIndexOf(id);
-        }
     }
     if (classes.length == 0) {
         let node = document.createTextNode("You do not have any classes today! :)");
         let para = document.createElement("p");
         para.appendChild(node);
-        let element = document.getElementById("div2");
+        let element = document.getElementById("today");
         element.appendChild(para);
     }
 } 
 function getScheduleTommorow(classes, date) {
-    titleNode = document.createTextNode("Next School Day's Classes:");
+    titleNode = document.createTextNode("Next Day's Classes:");
     title = document.createElement("h1");
     title.appendChild(titleNode);
-    var node1 = document.createTextNode("Classes you have:");
-    var para1 = document.createElement("h3");
-    para1.appendChild(node1);
-    var element1 = document.getElementById("div3");
+    var element1 = document.getElementById("tomorrow");
     element1.appendChild(title);
-    element1.appendChild(para1);
+    for (i = 0; i < classes.length; i++) {
+        var spaceNode = document.createTextNode(" ");
+        var space = document.createElement("p");
+        space.appendChild(spaceNode);
+        var Break = document.createElement("br");
+        var node = document.createTextNode("Period " + classes[i].charAt(classes[i].length-1) + " - " + classes[i].substring(0, classes[i].length-5));
+        var element = document.getElementById("tomorrow");
+        element.appendChild(node);
+        element.appendChild(Break);
+        element.appendChild(space);
+        element.appendChild(Break);
+    }
+    if (classes.length == 0) {
+        let node = document.createTextNode("IT'S THE END OF THE SCHOOL YEAR!!!!!");
+        let para = document.createElement("p");
+        para.appendChild(node);
+        let element = document.getElementById("today");
+        element.appendChild(para);
+    }
+    homework(classes, date);
+}
+
+function homework(classes, date) {
+    titleNode = document.createTextNode("Homework:");
+    title = document.createElement("h1");
+    title.appendChild(titleNode);
+    var element1 = document.getElementById("homework");
+    element1.appendChild(title);
     for (i = 0; i < classes.length; i++) {
         var spaceNode = document.createTextNode(" ");
         var space = document.createElement("p");
@@ -97,8 +103,8 @@ function getScheduleTommorow(classes, date) {
         id = "tommorowCheckbox" + i;
         checkbox.setAttribute("id", id);
         checkbox.setAttribute("onclick", "writeCookie(" + "\"tommorowCheckbox" + i + "\", " + "\"" + date.toUTCString() + "\")");
-        var node = document.createTextNode(classes[i]);
-        var element = document.getElementById("div3");
+        var node = document.createTextNode("Period " + classes[i].charAt(classes[i].length-1) + " - " + classes[i].substring(0, classes[i].length-5));
+        var element = document.getElementById("homework");
         element.appendChild(node);
         element.appendChild(checkbox);
         element.appendChild(Break);
@@ -118,10 +124,10 @@ function getScheduleTommorow(classes, date) {
         let node = document.createTextNode("IT'S THE END OF THE SCHOOL YEAR!!!!!");
         let para = document.createElement("p");
         para.appendChild(node);
-        let element = document.getElementById("div2");
+        let element = document.getElementById("today");
         element.appendChild(para);
     }
-} 
+}
 
 function writeCookie(elementID) {
     checkbox = document.getElementById(elementID);
