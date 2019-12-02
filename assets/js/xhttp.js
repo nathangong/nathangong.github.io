6a3612dc400b683289a31e2571fa2b93892933dc
nathangong <nathangong1234@gmail.com>
2019-12-02 15:00:36 -0800

    Fix cookie expiration date

--- assets/js/xhttp.js
+++ assets/js/xhttp.js
@@ -99,7 +99,6 @@ function getClassesTommorow(json) {
     }
     var start = json.value[0].start.dateTime;
     var date = new Date(start);
-    date.setDate(date.getDate() + 1);
     date.setHours(0, 0, 0, 0);
     getScheduleTommorow(classes, date);
 }
